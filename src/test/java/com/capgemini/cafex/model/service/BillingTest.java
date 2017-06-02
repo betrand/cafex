b226b916704fe46f62a16c70c891a6790c532e46
bu <bu@bu.mshome.net>
2017-06-02 05:36:49 +0100

    Test that getBill with Unknown Item returns message Invalid Item

--- src/test/java/com/capgemini/cafex/model/service/BillingTest.java
+++ src/test/java/com/capgemini/cafex/model/service/BillingTest.java
@@ -14,9 +14,21 @@ public class BillingTest {
   *
   */
  @Test
- public void testGetBillEmptyList(){
+ public void testGetBillEmptyList() {
   String[] purchaseItems = new String[0];
   String expResult = "0p";
+  String result = Billing.getBill(purchaseItems);
+  assertEquals(expResult, result);
+ }
+
+ /**
+  * Testing that a String list containing an Unknown Item returns Invalid Item
+  *
+  */
+ @Test
+ public void testGetStandardBillForUnknownItem() {
+  String[] purchaseItems = {"Red Bull"};
+  String expResult = "Invalid Item";
   String result = Billing.getBill(purchaseItems);
   assertEquals(expResult, result);
  }
