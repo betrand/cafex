9f4fa5ebfb6cde32cd99c494b3647b7a8cb09f99
bu <bu@bu.mshome.net>
2017-06-02 05:04:14 +0100

    Added Main Class with Main method

---
+++ src/main/java/com/capgemini/cafex/control/Main.java
@@ -0,0 +1,15 @@
+package com.capgemini.cafex.control;
+
+import java.util.Arrays;
+
+/**
+ *
+ * @author betrand.ugorji@capgemini.com
+ */
+public class Main {
+
+ public static void main(String[] args) {
+  System.out.println(Arrays.toString(args));
+ }
+
+}
